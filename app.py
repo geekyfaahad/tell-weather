b0e00a4ff5e8c4bd4f9770ca6c00bb066df9c0ea
geekyfaahad <77067677+geekyfaahad@users.noreply.github.com>
2022-10-02 11:49:56 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,request,redirect,url_for
+import requests,json
+
+app= Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/output', methods=['POST','GET'])
+def view():
+    try:
+        if request.method=='POST':
+            city=str(request.form['city'])
+            base_url='https://api.openweathermap.org/data/2.5/weather?q='
+            city=city
+            api=open('api-key').read()
+            url=(f'{base_url}{city}&appid={api}')
+            response=json.loads(requests.request("GET", url).text)
+            temp=(response['main']['temp'])
+            c=int(temp-273.15)
+            return render_template("output.html",result=c,city=city) 
+        elif request.method=='GET':
+            return "sorry we don't accept get requests"
+    except:
+        return redirect(url_for('index'))
+app.run(debug=True)
