07967f941000223739fb9d67365d35b1894de300
geekyfaahad <77067677+geekyfaahad@users.noreply.github.com>
2024-10-20 20:36:10 +0530

    Update api_secret.py

--- api_secret.py
+++ api_secret.py
@@ -1,3 +1,9 @@
+#This Api is only for viewing/testing the project
+#PLease Don't spam it 
+
+
 UNSPLASH_ACCESS_KEY='sjWaq9YcnmOfZQfZsDls0Uk6uppmn7yzmO8riMdoHgY'
 UNSPLASH_SECRET_KEY='5Rj8BCrqkE2_ADDIwbIw1nL5MMkpaKNWb37cem2aGNc'
 WEATHER_API_KEY='b4d2e359c5743907bba01667baeabae9'
+
+
